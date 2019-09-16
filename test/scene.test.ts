add6b48d62e79a823abc6d44f19a0b49418d12a1
florisdh <florisdh@gmail.com>
2019-09-16 22:13:49 +0200

    Added hasRun tests

--- test/scene.test.ts
+++ test/scene.test.ts
@@ -9,6 +9,10 @@ describe('Scene', () => {
 
     it('contains the right properties', () => {
         expect(scene).toHaveProperty('app');
+        expect(scene.app).toBe(null);
         expect(scene).toHaveProperty('scenes');
+        expect(scene.scenes).toBe(null);
+        expect(scene).toHaveProperty('hasRun');
+        expect(scene.hasRun).toBe(false);
     });
 });
